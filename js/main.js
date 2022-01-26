02de732c8aa2a6e6bb09e2b32dd83c3621d4e592
Sven Sonnborn <sonnborn.sven@web.de>
2022-01-26 16:52:47 +0100

    First commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 /*
 Das soll die Main JS sein :) und gleichzeitig ein Test
 */
+
+let tasks =[
+     {
+        "Titel":"Mangement", 
+        "Category":"Doe",
+        "Description":"Test",
+        "Due Date":"02.70.22",
+        "Urgency":"High",
+        "Assigned":"Peter",
+//        "placed":"backlog"
+     },
+     {
+      "Titel":"Mangement", 
+      "Category":"Doe",
+      "Description":"Test",
+      "Due Date":"02.70.22",
+      "Urgency":"High",
+      "Assigned":"hans",
+//      "placed":"backlog"
+   },     
+   {
+      "Titel":"Mangement", 
+      "Category":"Doe",
+      "Description":"Test",
+      "Due Date":"02.70.22",
+      "Urgency":"High",
+      "Assigned":"Peter",
+//      "placed":"backlog"
+   }
+
+   ]
+   
