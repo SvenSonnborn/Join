6e23b14e0a875219c53476a8ca3780490f4d097e
Vilia <vzeisig@gmail.com>
2022-01-30 10:00:54 +0100

    corrected syntax mistake in main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function includeHTML() {
         if (file) {
             /* Make an HTTP request using the attribute value as the file name: */
             xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
+            xhttp.onreadystatechange = function () {
                 if (this.readyState == 4) {
                     if (this.status == 200) { elmnt.innerHTML = this.responseText; }
                     if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
@@ -30,31 +30,39 @@ function includeHTML() {
 }
 
 let tasks = [{
-        "Titel": "Mangement",
-        "Category": "Doe",
-        "Description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-        "Due Date": "02.70.22",
-        "Urgency": "High",
-        "Assigned": "Peter",
-        //        "placed":"backlog"
-    },
-    {
-        "Titel": "Mangement",
-        "Category": "Doe",
-        "Description": "Test",
-        "Due Date": "02.70.22",
-        "Urgency": "High",
-        "Assigned": "hans",
-        //      "placed":"backlog"
-    },
-    {
-        "Titel": "Mangement",
-        "Category": "Doe",
-        "Description": "Test",
-        "Due Date": "02.70.22",
-        "Urgency": "High",
-        "Assigned": "Peter",
-        //      "placed":"backlog"
-    }
+    "Titel": "Mangement",
+    "Category": "Doe",
+    "Description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    "Due Date": "02.03.22",
+    "Urgency": "High",
+    "Assigned": "Peter",
+    "UserImage": "img/profile4.png",
+    "Status": "backlog" // >> Locaction of current task"backlog", "ToDo", "inProgress", "testing", "done"  
+    //        "placed":"backlog"  >> we can change that into "status" (see above)
+},
+{
+    "Titel": "Social Media",
+    "Category": "Marketing",
+    "Description": "This is a short task description for Task 2 in Project Management",
+    "Due Date": "02.07.22",
+    "Urgency": "Low",
+    "Assigned": "Hans",
+    "Status": "backlog", // >> Locaction of current task"backlog", "ToDo", "inProgress", "testing", "done"  
+    "UserImage": "img/profile3.png"
+
+    //      "placed":"backlog"
+},
+{
+    "Titel": "Mangement",
+    "Category": "Doe",
+    "Description": "Run a test for your new developed software.",
+    "Due Date": "12.10.22",
+    "Urgency": "Medium",
+    "Assigned": "Vilia",
+    "Status": "backlog", // >> Locaction of current task"backlog", "ToDo", "inProgress", "testing", "done"  
+    "UserImage": "img/profile1.png",
+
+    //      "placed":"backlog"
+}
 
 ]
