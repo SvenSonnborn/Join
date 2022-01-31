10abba242be9416cf17ca74763044abd5eacac32
Vilia <vzeisig@gmail.com>
2022-01-31 09:46:39 +0100

    categories change border color & buttons

--- js/backlog.js
+++ js/backlog.js
@@ -1,6 +1,6 @@
 function initBacklog(){
     render();
-    addBorderColors();
+
 }
 
 function render(){
@@ -9,27 +9,56 @@ function render(){
 
     for (let i = 0; i < tasks.length; i++) {
         mainContainer.innerHTML += 
-        `<div class="taskContainer">
-                <div class="assignedTo d-flex-center">
-                    <img id="userImg" src="${tasks[i].UserImage}" alt="user Image">
-                    <div class="userData">
-                        <span id="userName">${tasks[i].Assigned}</span><br>
-                        <span id="userEmail">${tasks[i].Email}</span>
+        `<div class="taskContainer-wrapper d-flex-center">
+            <div class="taskContainer" id="task-${i}">
+                    <div class="assignedTo d-flex-center" >
+                        <img id="userImg" src="${tasks[i].UserImage}" alt="user Image">
+                        <div class="userData">
+                            <span id="userName">${tasks[i].Assigned}</span><br>
+                            <span id="userEmail">${tasks[i].Email}</span>
+                        </div>
                     </div>
-                </div>
 
-                <div class="taskTimeline d-flex-center"> 
-                    <span id="dueDate">
-                    <span class="material-icons">event</span>${tasks[i]["Due Date"]}</span>
-              </div>
-                <div class="taskDescription d-flex-center">${tasks[i].Description}</div>
+                    <div class="taskTimeline d-flex-center"> 
+                        <span id="dueDate">
+                        <span class="material-icons">event</span>${tasks[i]["Due Date"]}</span>
+                    </div>
+                    <div class="taskDescription d-flex-center">${tasks[i].Description}</div>
+            </div>
+
+            <div class="taskButtons d-flex-center">
+                <button id="btn-addBoard" onclick="addTaskToBoard(${i})"><span class="material-icons">
+                add
+                </span></button>
+                <button id="btn-edit" onclick="editTask(${i})"><span class="material-icons">
+                mode_edit_outline
+                </span></button>
+            </div>
+
         </div>
 `
-;    
+addBorderColors(i);    
     }
 }
 
 
-function addBorderColors() {
-console.log('here');
+function addBorderColors(i) {
+    if (tasks[i].Category == 'Marketing'){
+        let taskContainer = document.getElementById(`task-${i}`)
+        taskContainer.classList.add('border-green')  
+    }
+
+    if (tasks[i].Category == 'Doe'){
+        let taskContainer = document.getElementById(`task-${i}`)
+        taskContainer.classList.add('border-lila')  
+    }
+}
+
+function addTaskToBoard(i){
+console.log('addToBoard')
+}
+
+function editTask(i){
+    console.log('editTask')
+
 }
