5b1111ef38e65c5410936a6785f16b3a273a5dc6
Vilia <vzeisig@gmail.com>
2022-02-01 13:33:43 +0100

    archive and edit function in backlog

--- js/backlog.js
+++ js/backlog.js
@@ -15,7 +15,7 @@ function render() {
         for (let i = 0; i < backlogTasks.length; i++) {
             let currentTask = backlogTasks[i];
             mainContainer.innerHTML +=
-                `<div class="taskContainer-wrapper d-flex-center">
+                `<div id="taskContainer-${i}" class="taskContainer-wrapper d-flex-center">
             <div class="taskContainer" id="task-${i}">
                     <div class="assignedTo d-flex-center" >
                         <img id="userImg" src="${currentTask.Assigned.UserImage}" alt="user Image">
@@ -33,14 +33,19 @@ function render() {
                         <span id="urgency">
                             <div id="dueDate" class="material-icons">av_timer</div>${currentTask.Urgency}
                         </span>
-                        
                     </div>
-                    <div class="taskDescription d-flex-center">${currentTask.Description}</div>
+                    <div class="taskDescription d-flex-center">
+                        <div id="titleContainer-${i}" class="titleTask">${currentTask.Title}</div>
+                        <div id="textContainer-${i}">${currentTask.Description}</div>
+                    </div>
             </div>
 
             <div class="taskButtons d-flex-center">
                 <button id="btn-addBoard" onclick="addTaskToBoard(${i})"><span class="material-icons">
                 add
+                </span></button>
+                <button id="btn-delete" onclick="deleteTask(${i})"><span class="material-icons">
+                delete
                 </span></button>
                 <button id="btn-edit" onclick="editTask(${i})"><span class="material-icons">
                 mode_edit_outline
@@ -81,12 +86,33 @@ function addTaskToBoard(i) {
     render();
 }
 
-
-function editTask(i) {
-    console.log('editTask', i);
+function deleteTask(i){
+    let taskToDelete = backlogTasks[i];
+    taskToDelete.Status = "archived";
     render();
 }
 
+function editTask(i) { 
+
+    console.log('edit', i)
+
+let titleContainer = document.getElementById(`titleContainer-${i}`);
+titleContainer.setAttribute("contenteditable","true");
+titleContainer.focus();
+let textContainer = document.getElementById(`textContainer-${i}`);
+textContainer.setAttribute("contenteditable","true");
+
+
+}
+
+function makeCalEditable(){
+    console.log('changeDate')
+
+}
+
+function changeUser(){
+    console.log('changeUser')
+}
 
 function showNoteEmptyBacklog() {
     let container = document.getElementById('msgEmptyBacklog')
