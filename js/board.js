a7ce8e4e1a87051809943d9a7669355e02b88360
Vilia <vzeisig@gmail.com>
2022-02-16 09:53:32 +0100

    JSDOC Board.js

--- js/board.js
+++ js/board.js
@@ -3,13 +3,26 @@ let inprogressTasks;
 let testTasks;
 let doneTasks;
 
+/**
+ * Get data from storage 
+ */
 async function init() {
   await downloadFromServer();
-  tasks = JSON.parse(backend.getItem("tasks")) || []; //Danke Sven :)
+  tasks = JSON.parse(backend.getItem("tasks")) || [];
   includeHTML();
   render();
 }
 
+/**
+ * Save data to storage 
+ */
+ async function changeStatus() {
+  await backend.setItem("tasks", JSON.stringify(tasks));
+}
+
+/**
+ * MAIN RENDER: display all tasks according to status 
+ */
 function render() {
   document.getElementById("toDo").innerHTML = "";
   document.getElementById("inProgress").innerHTML = "";
@@ -20,19 +33,25 @@ function render() {
     let currentTask = tasks[i];
     let currentTaskStatus = currentTask.Status;
     if (currentTaskStatus === "toDo" || currentTaskStatus === "inProgress" || currentTaskStatus === "test" || currentTaskStatus === "done") {
-          document.getElementById(currentTaskStatus).innerHTML +=
-        `<div draggable="true" ondragstart="drag(event)" id="task-${i}" class="singleTask">
-                <h4>${currentTask.Title}</h4>
-                <a onclick="ShowOverlay(${i})">Show detailed</a>
-            </div>
-            `;
+          document.getElementById(currentTaskStatus).innerHTML += generateTaskHTML(i);
+        ;
       addBackgroundColor(currentTask, i);
     }
   }
 }
 
-///////// DRAG AND DROP ////////
+function generateTaskHTML(i){
+return `<div draggable="true" ondragstart="drag(event)" id="task-${i}" class="singleTask">
+<h4>${tasks[i].Title}</h4>
+<a onclick="ShowOverlay(${i})">Show detailed</a>
+</div>
+`}
 
+
+/**
+ * Drag and Drop functions adapted from W3schools
+ * 
+ */
 function allowDrop(ev) {
   ev.preventDefault();
 }
@@ -46,10 +65,15 @@ function moveTo(ev) {
   let data = ev.dataTransfer.getData("text");
   let targetDiv = ev.target;
   ev.target.appendChild(document.getElementById(data));  // data is the ID of the element that is dragged, in our case task-${i}
-  console.log(ev.target);
   changeStatusAfterDragging(data, targetDiv);
 }
 
+
+/**
+ * Update Status after task is dragged into another column
+ * @taskName is the Id of the HTML Element of the current task (this id consists of a string, attached to an index)
+ * @targetDiv is the Board column as a DIV element,where the current taks is dropped
+ */
 function changeStatusAfterDragging(taskName, targetDiv) {
   let indexCurrentTask = taskName.replace(/\D/g,'');  // extracts the number from a string (e.g. taskName is task-1) to get the index >> 1
   let newStatus = targetDiv.id;
@@ -57,9 +81,13 @@ function changeStatusAfterDragging(taskName, targetDiv) {
   changeStatus();  // saves to backend
   render();
 }
-/////////////////
 
 
+/**
+ * Update Status after task is dragged into another column
+ * @currentTask {object}
+ * @i {number}
+ */
 function addBackgroundColor(currentTask, i) {
   let taskContainer = document.getElementById(`task-${i}`);
   if (currentTask.Category == "Marketing") {
@@ -76,20 +104,35 @@ function addBackgroundColor(currentTask, i) {
   }
 }
 
+/**
+ * Show task in detail view --> Overlay creating 
+ * @i {number}
+ */
 function ShowOverlay(i) {
   document.getElementById("Descriptionoverlay").classList.remove("d-none");
   RenderDescription(i);
 }
-
+/**
+ * Hide task in detail view --> Overlay closing 
+ * @i {number}
+ */
 function CloseOverlay() {
   document.getElementById("Descriptionoverlay").classList.add("d-none");
   render();
 }
 
+/**
+ * Show task in detail view  
+ * @i {number}
+ */
 function RenderDescription(i) {
   let selection = document.getElementById("Descriptionbox");
+  selection.innerHTML = generateDetailTaskHTML(i);
+}
+
+function generateDetailTaskHTML(i){
   let currentTask = tasks[i];
-  selection.innerHTML = `
+return`
           <div class="innerDescription">
                 <h3 class="whitecolor textCenter"><u>Titel:</u><br></h3> 
                 <h4>${currentTask.Title}</h4>
@@ -116,6 +159,10 @@ function RenderDescription(i) {
           `;
 }
 
+/**
+ * Update Task after StatusChange on ButtonClick inside detail view of task is opened
+ * @i {number}
+ *  */
 function UpdateTask(i) {
   let StatusChange = document.getElementById("Status").value;
   tasks[i].Status = StatusChange;
@@ -124,20 +171,6 @@ function UpdateTask(i) {
   render();
 }
 
-async function changeStatus() {
-  await backend.setItem("tasks", JSON.stringify(tasks));
-}
 
 
 
-
-
-// function checkEmptyTodo() {
-//   if (todoTasks === 0) {
-//     showMsgEmptyTodo();
-//   }
-// }
-
-// function showMsgEmptyTodo() {
-//   return `<div>This Board is Empty</div>`;
-// }
